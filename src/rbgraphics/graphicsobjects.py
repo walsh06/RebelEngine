480db75c7a521e5bd5d19e54813f35c353c4ada3
Liam Walsh <liammail06@gmail.com>
2015-07-23 22:30:37 +0100

    Fixed undraw graphic objects logic

--- src/rbgraphics/graphicsobjects.py
+++ src/rbgraphics/graphicsobjects.py
@@ -2,6 +2,7 @@ import Tkinter as tk
 
 from graphics import _root
 
+
 class GraphicObject(object):
 
 	def __init__(self, x=0, y=0):
@@ -11,15 +12,20 @@ class GraphicObject(object):
 		self._x = x
 		self._y = y
 
-class rbImage(GraphicObject):
+	def undraw(self, canvas):
+		canvas.remove(self)
+		self._id = None
 
-	def __init__(self, image, x = 0, y = 0):
-		super(rbImage, self).__init__(x, y)
+
+class RBImage(GraphicObject):
+
+	def __init__(self, image, x=0, y=0):
+		super(RBImage, self).__init__(x, y)
 		self._img = tk.PhotoImage(file=image, master=_root)
 		self._id = None
 
 	def draw(self, canvas, x, y):
-		if self._id == None:	
+		if self._id is None:
 			self._id = canvas.create_image(x, y, image=self._img)
 			canvas.update()
 		else:
@@ -29,13 +35,11 @@ class rbImage(GraphicObject):
 			self.setObjectPos(x, y)
 		_root.update()
 
-	def undraw(self):
-		self._id = None
 
-class rbText(GraphicObject):
+class RBText(GraphicObject):
 
 	def __init__(self, text, x, y):
-		super(rbText, self).__init__(x, y)
+		super(RBText, self).__init__(x, y)
 		self._id = None
 		self._text = text
 		self._textChange = False
@@ -45,9 +49,9 @@ class rbText(GraphicObject):
 		self._textChange = True
 
 	def draw(self, canvas):
-		if self._id == None:
+		if self._id is None:
 			self._id = canvas.create_text(self._x, self._y, {"text": self._text})
-		elif self._textChange == True:
+		elif self._textChange is True:
 			canvas.delete(self._id)
 			self._id = canvas.create_text(self._x, self._y, {"text": self._text})
 			self._textChange = False
