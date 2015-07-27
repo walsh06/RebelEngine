91941ebaa182c387dd27aca6f5ffab0f3749f6cf
Liam Walsh <liammail06@gmail.com>
2015-07-27 21:58:01 +0100

    Updated behaviour to use movePos and adjustable speeds

--- src/rbai/rbbehaviour.py
+++ src/rbai/rbbehaviour.py
@@ -1,16 +1,25 @@
 class RBMoveBehaviourBase(object):
 
+	def __init__(self, speed):
+		self._speed = speed
+
 	def updatePosition(self, pos):
 		pass
 
 
 class RBMoveDown(RBMoveBehaviourBase):
 
+	def __init__(self, speed):
+		super(RBMoveDown, self).__init__(speed)
+
 	def updatePosition(self, pos):
-		pos.setPos(pos.getX(), pos.getY() + 1)
+		pos.movePos(0, self._speed)
 
 
 class RBMoveUp(RBMoveBehaviourBase):
 
+	def __init__(self, speed):
+		super(RBMoveUp, self).__init__(speed)
+
 	def updatePosition(self, pos):
-		pos.setPos(pos.getX(), pos.getY() - 1)
+		pos.movePos(0, -self._speed)
