4f4963ae4e33c27cbe018149742b0cbe44320c7b
Liam Walsh <liammail06@gmail.com>
2026-08-12 10:21:07 +0100

    Update tests to new RBGame

--- test/testSound.py
+++ test/testSound.py
@@ -14,7 +14,6 @@ class TestGame(RBGame):
 
     def __init__(self):
         super(TestGame, self).__init__()
-        self.testSound = RBSound("test.wav")
         self.initGraphics(100, 100)
         self.testGraphics = self._graphics
         self.initController()
@@ -22,22 +21,21 @@ class TestGame(RBGame):
         self.testController.registerKeyFunction("q", self.quit)
         self.testController.registerKeyFunction("space", self.playSound)
         self.testText = RBText("TEST", RB2DPosition(100, 50))
-        self._running = True
+        self.testSound = RBSound("test.wav")
+        self.count = 0
 
-    def update(self):
-        count = 0
-        while self._running:
-            super(TestGame, self).update()
-            count += 1
+    def onUpdate(self):
+        self.count += 1
 
-            self.testText.draw(self.testGraphics)
+    def onDraw(self):
+        self.testText.draw(self.testGraphics)
 
     def playSound(self):
-        tempSound = RBTempSound("test.wav")
+        RBTempSound("test.wav")
 
     def quit(self):
         self._running = False
 
 if __name__ == "__main__":
     test = TestGame()
-    test.update()
+    test.run()
